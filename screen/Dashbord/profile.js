9d7bd24d576dbc8a429976ac3e109e7363ddd149
Rahvbht99 <raghvbhatia99@icloud.com>
2022-10-01 13:14:42 +0500

    Attending

--- screen/Dashbord/profile.js
+++ screen/Dashbord/profile.js
@@ -93,10 +93,181 @@ const Card= (props) => {
                         </TouchableOpacity>
                     </LinearGradient>
                     <LinearGradient
+                        colors={GRADIENTS.success}
+                        end={{ x: 0, y: 1 }}
+                        start={{ x: 1, y: 0 }}
+                        style={{ borderRadius: 30, marginLeft: '10%' }}
+                    >
+                        <TouchableOpacity
+                            style={{
+                                alignItems: "center",
+                                padding: 10,
+                                flexDirection: "row",
+                            }}
+                            onPress={() => {
+                                deleteEvent(data).then((item) => {
+                                    alert("Event deleted successfully!");
+                                    navigation.push('Dashbord');
+                                }).catch((err) => {
+                                    alert(err.message);
+                                })
+                            }}
+                        >
+                            <MaterialIcons name="delete" size={24} color="black" />
+                            <Text style={{ color: 'black' }}>Edit</Text>
+                        </TouchableOpacity>
+                    </LinearGradient>
+                    <LinearGradient
                         colors={GRADIENTS.secondary}
                         end={{ x: 0, y: 1 }}
                         start={{ x: 1, y: 0 }}
-                        style={{ borderRadius: 30, marginLeft: '40%' }}
+                        style={{ borderRadius: 30, marginLeft: '20%' }}
+                    >
+                        <TouchableOpacity
+                            style={{
+                                alignItems: "center",
+                                padding: 10,
+                                flexDirection: "row",
+                            }}
+                            onPress={() => {
+                                deleteEvent(data).then((item) => {
+                                    alert("Event deleted successfully!");
+                                    navigation.push('Dashbord');
+                                }).catch((err) => {
+                                    alert(err.message);
+                                })
+                            }}
+                        >
+                            <MaterialIcons name="delete" size={24} color="black" />
+                            <Text style={{ color: 'black' }}>Delete</Text>
+                        </TouchableOpacity>
+                    </LinearGradient>
+                </View>
+            </View>
+            <Modal
+                animationType="slide"
+                transparent={true}
+                visible={modalVisible}
+                onRequestClose={() => {
+                    setModalVisible(!modalVisible);
+                }}
+            >
+                <View style={styles.centeredView}>
+                    <View style={styles.modalView}>
+                        <View style={{ margin: 5 }}>
+                            <TouchableOpacity onPress={() => setModalVisible(false)}>
+                                <Image
+                                    style={{
+                                        width: 20,
+                                        height: 20,
+                                        borderRadius: 10
+                                    }}
+                                    source={require('../../assets/icons/close-icon.png')}
+                                />
+                            </TouchableOpacity>
+                        </View>
+                        {/* ====================================================================================== */}
+                        <View style={styles.modalContainer}>
+                            <FlatList
+                                data={data.attending}
+                                keyExtractor={({ item, index }) => index}
+                                renderItem={({ item, index, separators }) => (
+                                    <View key={index} style={{ flexDirection: 'row', padding: 15, marginBottom: 10, backgroundColor: '#D8D6E6', borderRadius: 30 }}>
+                                        <Avatar.Image
+                                            source={{
+                                                uri: item.photoUrl
+                                            }}
+                                            size={80}
+                                        />
+                                        <View>
+                                            <View style={{ marginLeft: 20 }}>
+                                                <Title
+                                                    style={
+                                                        {
+                                                            marginTop: 15,
+
+                                                            color: 'black'
+                                                        }
+                                                    }
+                                                >
+                                                    {item.name}
+                                                </Title>
+                                            </View>
+                                            <View style={{ marginLeft: 20 }}>
+                                                <Title
+                                                    style={
+                                                        {
+                                                            color: 'black'
+                                                        }
+                                                    }
+                                                >
+                                                    {item.email}
+                                                </Title>
+                                            </View>
+                                        </View>
+                                    </View>
+                                )}
+                            />
+
+                        </View>
+                        {/* ====================================================================================== */}
+                    </View>
+                </View>
+            </Modal>
+        </View>
+    )
+}
+const CardAttending= (props) => {
+    // props.data.index
+    // { data, deleteEvent, navigation ,key}
+    let data =props.data.item;
+    let deleteEvent=props.data.deleteEvent;
+    let navigation=props.data.navigation;
+    let key=props.data.key;
+
+    const [modalVisible, setModalVisible] = useState(false);
+    console.log(data);
+    return (
+        <View key={key} style={styles.card}>
+            <View style={styles.EventDetails}>
+                <View style={{ justifyContent: 'center', alignItems: 'center', margin: 5 }}>
+                    <Text style={{ color: '#000000', fontSize: 22, fontWeight: 'bold' }}>{data.name}</Text>
+                </View>
+                <View style={{ flexDirection: 'row' }}>
+                    <Icon name="update" size={24} color="black" />
+                    <Text style={{ fontSize: 18, color: 'black', marginLeft: 10 }}>
+                        Date:{data.date}
+                    </Text>
+                </View>
+                <View style={{ flexDirection: 'row' }}>
+                    <Icon name="camera-timer" size={24} color="black" />
+                    <Text style={{ fontSize: 18, color: 'black', marginLeft: 10 }}>
+                        Time:{data.time}
+                    </Text>
+                </View>
+                <View style={{ flexDirection: 'row' }}>
+                    {/* <Icon name="location-enter" size={24} color="black" /> */}
+                    <Ionicons name="location" size={24} color="black" />
+                    <Text style={{ fontSize: 18, color: 'black', marginLeft: 10 }}>
+                        Address: {data.address}
+                    </Text>
+                </View>
+                <View >
+                    <View style={{ flexDirection: 'row', paddingBottom: 10 }}>
+                        {/* <Icon name="location-enter" size={24} color="black" /> */}
+                        <MaterialIcons name="description" size={24} color="black" />
+                        <Text style={{ fontSize: 18, color: 'black', marginLeft: 10 }}>
+                            Description:
+                        </Text>
+                    </View>
+                    <Text>{data.description}</Text>
+                </View>
+                <View style={{ flexDirection: 'row', marginTop: 10, flex: 1 }}>
+                    <LinearGradient
+                        colors={GRADIENTS.secondary}
+                        end={{ x: 0, y: 1 }}
+                        start={{ x: 1, y: 0 }}
+                        style={{ borderRadius: 30}}
                     >
                         <TouchableOpacity
                             style={{
@@ -193,14 +364,6 @@ const Card= (props) => {
     )
 }
 
-// const Card =( props) => {
-//     console.log(props.data.item)
-//     let data =props.data.item;
-//     let deleteEvent=props.data.deleteEvent;
-//     let navigation=props.data.navigation;
-//     let key=props.data.key;
-//     return (<Text style={{color:'white'}}>hello</Text>)
-// }
 const Profile = ({ navigation }) => {
     const { getMyEvent, deleteEvent } = useData();
     const [event, setEvent] = useState([]);
@@ -267,7 +430,16 @@ const Profile = ({ navigation }) => {
                                 <View style={{ marginTop: 10 }}>
                                     {event.map((item, index) => {
                                         console.log(index)
-                                        return (<Card data={{item,index,navigation,deleteEvent}} />);
+                                        return (<Card key={index} data={{item,index,navigation,deleteEvent}} />);
+                                    })}
+                                </View>
+                            </View>
+                            <View style={styles.OrganizedEvent}>
+                                <Text style={{ fontSize: 30, color: 'white' }}>Attending Event</Text>
+                                <View style={{ marginTop: 10 }}>
+                                    {event.map((item, index) => {
+                                        console.log(index)
+                                        return (<CardAttending key={index} data={{item,index,navigation,deleteEvent}} />);
                                     })}
                                 </View>
                             </View>
