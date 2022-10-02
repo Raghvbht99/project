baa3ce18503a632f7da9f07ecf9d29a72280cb2d
Rahvbht99 <raghvbhatia99@icloud.com>
2022-10-02 15:49:16 +0500

    Update Event

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ function App() {
     <Stack.Navigator screenOptions={{
       headerShown: false
     }}
-      initialRouteName="Dashbord">
+      initialRouteName="StartingPoint">
       <Stack.Screen name="StartingPoint" component={StartingPoint} />
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="SignUp" component={SignUp} />
